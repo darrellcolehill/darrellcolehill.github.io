bc127fdd96d49586c690c029592b7d1b7b1568c2
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-06-21 16:49:34 -0400

    Finished adding projects as a route

--- darrellcolehill/src/main.tsx
+++ darrellcolehill/src/main.tsx
@@ -3,12 +3,14 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import DevLogs from './views/Devlogs';
+import Projects from './views/Projects';
 
 ReactDOM.render(
   <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="devlogs" element={<DevLogs />} />
+        <Route path="projects" element={<Projects />} />
       </Route>
     </Routes>
   </HashRouter>,
