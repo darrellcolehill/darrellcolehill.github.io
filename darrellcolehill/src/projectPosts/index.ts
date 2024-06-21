ed97f3337345d92f48a344ee87b0a3bf3db4c919
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-06-21 17:43:13 -0400

    Added post with type

---
+++ darrellcolehill/src/projectPosts/index.ts
@@ -0,0 +1,5 @@
+
+import { post as trashBikePost } from "./trashBike"
+export default {
+    "trashBike": trashBikePost
+}
---
+++ darrellcolehill/types.ts
@@ -0,0 +1,13 @@
+
+
+type ProjectPost = {
+    date: string;
+    title: string;
+    summary: string;
+    content: string;
+    chips?: string[];
+  };
+
+export type {
+    ProjectPost,
+}
