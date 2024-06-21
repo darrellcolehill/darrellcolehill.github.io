3dfcbf90a0518f516ef1884f4e5e4331989be8fa
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-06-21 16:49:49 -0400

    Update Outlined card for project posts

--- darrellcolehill/src/components/OutlinedCard.tsx
+++ darrellcolehill/src/components/OutlinedCard.tsx
@@ -1,64 +1,57 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Grid } from '@mui/material';
+import { CardHeader, Grid } from '@mui/material';
+import ReactMarkdown from 'react-markdown';
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+const markdown = `
+**Overview:** This project involves transforming a bike I found in the garbage into an interactive game controller by integrating various sensors with an Arduino microcontroller.
+
+**Key Features:**
+- **RPM Detection**: Measures the bike's speed to control the game's velocity parameters.
+- **Handle Bar Position Detection**: Detects changes in the handlebar position and maps it to left/right player movement
+- **Minimally Invasive**: Can use bike normally should I decide that I want to actually touch grass`
 
 const card = (
   <React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
-      </Typography>
-      <Typography variant="h5" component="div">
-        be{bull}nev{bull}o{bull}lent
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
+    <CardHeader
+      title={
+        <Typography variant="h5" component="div" sx={{ color: '#2C3E50' }}>
+          Smart Trash Bike
+        </Typography>
+      }
+      subheader={
+        <Typography variant="body2" component="div" sx={{ color: '#7F8C8D' }}>
+          June 21, 2024
+        </Typography>
+      }
+      sx={{ backgroundColor: '#ECF0F1' }}
+    />
+      <CardContent>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </CardContent>
+    <CardActions sx={{ justifyContent: 'flex-end' }}>
+      <Button size="small" sx={{ color: '#3498DB' }}>Learn More</Button>
     </CardActions>
   </React.Fragment>
 );
 
 export default function OutlinedCard() {
   return (
-    <Grid container>
+    <Grid container justifyContent="center" sx={{ padding: 2 }}>
       <Grid item xs={12}>
-      <Box
-          sx={{
-            minWidth: {
-              xs: 300, 
-              sm: 600,
-              md: 900, 
-              lg: 1200, 
-            },
-            margin: 'auto',
-          }}
-        >          <Card variant="outlined">
+        {/* <Box> */}
+          <Card variant="outlined">
             {card}
           </Card>
-        </Box>
+        {/* </Box> */}
       </Grid>
     </Grid>
   );
 }
+
+
+
