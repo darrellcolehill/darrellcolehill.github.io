894c2c7e53de59408345ab33968a35da55168395
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-06-21 16:48:44 -0400

    Uses React.Fragment instead of empty tags

--- darrellcolehill/src/App.tsx
+++ darrellcolehill/src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css'
 import { Outlet } from 'react-router-dom';
 import DrawerAppBar from './components/DrawerAppBar';
+import React from 'react';
 
 function App() {
 
   return (
-    <>
+    <React.Fragment>
       <DrawerAppBar/>
       <Outlet />
-    </>
+    </React.Fragment>
   )
 }
 
