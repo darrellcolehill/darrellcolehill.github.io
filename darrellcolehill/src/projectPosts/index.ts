aeef4aa8325770291e82d039b7af6c0316fb3b7a
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-06-24 16:48:39 -0400

    Added UCF Experience The Game as a post

--- darrellcolehill/src/projectPosts/index.ts
+++ darrellcolehill/src/projectPosts/index.ts
@@ -1,5 +1,7 @@
 
 import { post as trashBikePost } from "./trashBike"
+import { post as UCFExperienceTheGame} from "./UCFExperienceTheGame"
 export default {
-    "trashBike": trashBikePost
+    "trashBike": trashBikePost,
+    "ucfExperience": UCFExperienceTheGame,
 }
