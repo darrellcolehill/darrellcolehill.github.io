9d6f8957ff28bec25c0b39a1b7cae3562512ed06
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-06-27 09:27:07 -0400

    Added Catapult v2 idea

---
+++ darrellcolehill/src/projectPosts/voiceActivatedToiletPaperCatapultV2.ts
@@ -0,0 +1,20 @@
+import { ProjectPost } from "../../types";
+
+const post: ProjectPost = {
+    title: "Voice Activated Toilet Paper Catapult v2",
+    date: "June 24, 2024",
+    content: 
+        `**Overview:** same as version 1, but now an air cannon or uses fire crackers.
+
+**Key Features:**
+- **Deliver TP at high speed:** when you gotta go, you gotta got
+
+**Stretch Goals:**
+- **Face Following:**: Use OpenCV to implement basically a line following robot, but instead of following a line, it follows faces
+`,
+    summary: 
+        `**Overview:** same as version 1, but now an air cannon.`,
+    chips: ["3D Print", "OpenCV", "Robotics", "AWS", "Node.js", "Arduino", "RPI"]
+}
+
+export {post};
